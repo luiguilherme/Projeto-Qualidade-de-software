b6ea4013b48701367441f8a8f21296aba17b7693
Estheven Barroqueiro De Carvalho <estheven.carvalho@telefonica.com>
2023-09-19 10:55:38 -0300

    Revert "Merge branch 'ft-PTI1094-3368-272-280-284_v1/release' into 'release'"
    This reverts merge request !3258

--- force-app/main/default/lwc/cfFCchild_panFilterDateSMS/styleDefinition.js
+++
@@ -1,3 +0,0 @@
-let styleDefinition =
-      {"state0element1block_element2":[{"conditions":"default","styleObject":{"class":"slds-col  condition-element slds-text-align_left slds-p-top_large  slds-size_2-of-12  ","style":"      \n         ","styleProperties":{}}}]};
-  export default styleDefinition
