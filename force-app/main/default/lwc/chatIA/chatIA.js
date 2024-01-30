21d8bd9b0656d5625416a006b7468a62e4f086cd
Luiz Guilherme de Souza <l.guilhermedesouza@gmail.com>
2024-01-30 14:49:15 -0300

    [PTI5111] - Fluxo Feedback

--- force-app/main/default/lwc/chatIA/chatIA.js
+++ force-app/main/default/lwc/chatIA/chatIA.js
@@ -14,33 +14,46 @@ import addConversation from '@salesforce/apex/ChatIAController.AddConversation';
 import deleteChatById from '@salesforce/apex/ChatIAController.deleteChatById';
 import getQNAAnswer from '@salesforce/apex/ChatIAController.getQNAAnswer';
 import getPermission from '@salesforce/apex/ChatIAController.getPermission';
+import createNewHistory from '@salesforce/apex/ChatIAController.createNewHistory';
+
 
 import CHATIA_OBJECT from "@salesforce/schema/ChatIA__c";
 import ID_FIELD from "@salesforce/schema/ChatIA__c.Id";
 import BACKUP_FIELD from "@salesforce/schema/ChatIA__c.Backup__c";
 import USUARIO_FIELD from "@salesforce/schema/ChatIA__c.Usuario__c";
 import ACTIVE_FIELD from "@salesforce/schema/ChatIA__c.Active__c";
+import HISTORY_FIELD from "@salesforce/schema/ChatIA__c.History__c";
 
 export default class ChatIA extends NavigationMixin(LightningElement) {
     @track userId = userId;
     @track backupChat;
     @track chatIAId;
+    @track active ;
+    @track history ;
     @track mensagemInput;
+    @track correlator;
+    @track correlatorFeedback ='';
+    @track feedbackValue;
     @track conversaCompleta = [];
     @track questionSuggestions = [];
-    
+    @track lixeiraIsOpen = false;
     @track fieldsChatIAInsert = {};
     @track fieldsChatIAUpdate = {};
     
     //variáveis de controle
     @track thereIsPermission = true;
     @track loading = false;
+    @track dots = false;
     @track isOpen = false;
     @track buttomDisabled = false;
     @track suggestions = false;
+    @track feedbackIsOpen = false;
+    @track likeDisabled = false;
+    @track dislikeDisabled = false;
     @track tabIndex = '-1';
     @track heigthSize = 'height:98%';
     @track roleARIA = '';
+
     
     styleCSS = '<p style=\"align-items: flex-start; background-color: var(--colorIA, #660099); color: #ffffff; margin-left: -40px\; border-radius: 1.15rem; line-height: 1.25; max-width: 75%; padding: 0.5rem .875rem; position: relative; word-wrap: break-word">'
 
@@ -48,6 +61,20 @@ export default class ChatIA extends NavigationMixin(LightningElement) {
         let size = (window.screen.height *0.7) -110;
         this.heigthSize = 'height:'+ size.toString() +'px';
         this.verifyPermission();
+    }
+
+    
+    modalControl(event){
+        if(event.detail === 'cancelado'){
+            this.conversaCompleta.forEach(mensagem => {
+                if(mensagem.correlator === this.correlator){
+                    mensagem.disableLike = false;
+                    mensagem.disableDislike = false;
+                    mensagem.css = '';
+                }
+            })
+        }
+        this.feedbackIsOpen = false;
     }
     
     @api
@@ -92,13 +119,16 @@ export default class ChatIA extends NavigationMixin(LightningElement) {
     async startSessionId(){
         try{
             const data = await getSessionId();
-            for (let key in data) {
-                this.backupChat = key;
-                this.chatIAId = data[key];
+            if(data){
+                this.backupChat = data.backupChat;
+                this.chatIAId = data.recordFound;
+                this.active = data.active;
+                this.history = data.history;
             }
             if(this.chatIAId === 'null'){
                 this.fieldsChatIAInsert[USUARIO_FIELD.fieldApiName] = this.userId;
                 this.fieldsChatIAInsert[ACTIVE_FIELD.fieldApiName] = true;
+                this.fieldsChatIAInsert[HISTORY_FIELD.fieldApiName] = true;
                 this.roleARIA = 'alert';
             } else{
                 this.getChatHistory();
@@ -122,14 +152,28 @@ export default class ChatIA extends NavigationMixin(LightningElement) {
                     let mensagemUser = {
                         mensagemUsuario: hist.type === 'user' ? true : false,
                         conteudoMensagem: hist.message,
+                        correlator: hist.correlator,
+                        disableLike: false,
+                        disableDislike: false,
+                        css: '',
                     }
+                    if(hist.feedback === 'positive'){
+                        mensagemUser.disableDislike = 'true';
+                        mensagemUser.css = '--slds-c-icon-color-foreground: #660099;margin-left: -20px;';
+                    }
+                    if(hist.feedback === 'negative'){
+                        mensagemUser.disableLike = 'true';
+                        mensagemUser.css = '--slds-c-icon-color-foreground: #660099;margin-left: -20px;';
+                    }
+
             
                     this.conversaCompleta.push(mensagemUser);
                 });
                 this.suggestions = false;
             } else{
                 let closeConfirmModal = true;
-                this.clearHistory(closeConfirmModal);
+                let showToast = true;
+                this.clearHistory(closeConfirmModal,showToast);
             }
         } catch{
             this.dispatchEvent(new ShowToastEvent({
@@ -158,18 +202,20 @@ export default class ChatIA extends NavigationMixin(LightningElement) {
     async armazenarMensagem(event){
         try{
             this.buttomDisabled = true;
-            this.loading = true;
             this.suggestions = false;
             let mensagemVar = event.target.value ? event.target.value : this.template.querySelector('lightning-input[data-id="campoMensagem"]').value;
             this.template.querySelector('lightning-input[data-id="campoMensagem"]').value = null;
-    
             let mensagemUser = {
                 mensagemUsuario: true,
                 conteudoMensagem: mensagemVar,
+                correlator: '',
+                disableLike: false,
+                disableDislike: false,
+                css: '',
             }
-    
             this.conversaCompleta.push(mensagemUser);
-
+            this.dots = true;
+            this.forceScroll();
             const data = await getQNAAnswer({question: mensagemVar});
             if(data){
                 let urlDocument = '';
@@ -184,6 +230,10 @@ export default class ChatIA extends NavigationMixin(LightningElement) {
                 let mensagemIA = {
                     mensagemUsuario: false,
                     conteudoMensagem: this.styleCSS + data.content + urlDocument,
+                    correlator: data.correlator,
+                    disableLike: false,
+                    disableDislike: false,
+                    css: '',
                 }
                 
                 let conversaParcial = [];
@@ -191,20 +241,34 @@ export default class ChatIA extends NavigationMixin(LightningElement) {
                 if(this.chatIAId === 'null'){
                     conversaParcial.push(mensagemUser);
                     conversaParcial.push(mensagemIA);
-                    const newChat = await createNewChat({chatList: JSON.stringify(conversaParcial)});
-                    if(newChat){
-                        this.backupChat = newChat.x_id;
-                    } else{
-                        this.dispatchEvent(new ShowToastEvent({
-                            title: 'Erro',
-                            message: 'Não foi possível criar a sessão.',
-                            variant: 'error'
-                        }));
+                    if(this.history != 'null'){
+                        const newChat = await createNewChat({chatList: JSON.stringify(conversaParcial)});
+                        if(newChat){
+                            console.log('new chat ' + JSON.stringify(newChat));
+                            this.backupChat = newChat.x_id;
+                        } else{
+                            this.dispatchEvent(new ShowToastEvent({
+                                title: 'Erro',
+                                message: 'Não foi possível criar a sessão.',
+                                variant: 'error'
+                            }));
+                        }
+                    }else{
+                        const newChat = await createNewHistory({chatList: JSON.stringify(conversaParcial)});
+                        if(newChat){
+                            this.backupChat = newChat.x_id;
+                        } else{
+                            this.dispatchEvent(new ShowToastEvent({
+                                title: 'Erro',
+                                message: 'Não foi possível criar a sessão.',
+                                variant: 'error'
+                            }));
+                        }
                     }
-                    
                     this.fieldsChatIAInsert[USUARIO_FIELD.fieldApiName] = this.userId;
                     this.fieldsChatIAInsert[BACKUP_FIELD.fieldApiName] = this.backupChat;
                     this.fieldsChatIAInsert[ACTIVE_FIELD.fieldApiName] = true;
+                    this.fieldsChatIAInsert[HISTORY_FIELD.fieldApiName] = true;
                     const createChatIA = {
                         apiName: CHATIA_OBJECT.objectApiName,
                         fields: this.fieldsChatIAInsert
@@ -235,7 +299,7 @@ export default class ChatIA extends NavigationMixin(LightningElement) {
                 }
                 this.conversaCompleta.push(mensagemIA);
                 setTimeout(() => {
-                    this.loading = false;
+                    this.dots = false;
                     const inputScroll = this.template.querySelector('[data-id="scrollable"]');
                     inputScroll.scrollTop = inputScroll.scrollHeight;
                     this.buttomDisabled = false;
@@ -263,20 +327,20 @@ export default class ChatIA extends NavigationMixin(LightningElement) {
             }));
         }
     }
-    
-    
-    async clearHistory(closeConfirmModal){
-        let result;
-        if(closeConfirmModal != true){
-                result = await LightningConfirm.open({
-                message: "Deseja deletar o histórico do chat?",
-                variant: "headerless",
-                label: "Deletar histórico",
-                theme: 'info'
-            });
-        }
-
-        if(result || closeConfirmModal == true){
+    openLixeira(event){
+        this.lixeiraIsOpen = true;
+    }
+    clearHistoryEvent(event){
+       let result = event.detail
+       if(result ==='sucesso'){
+            this.clearHistory(true,false);
+            this.lixeiraIsOpen = false;
+       }else{
+        this.lixeiraIsOpen = false;
+       }
+    }
+    async clearHistory(closeConfirmModal, showToast){
+        if(closeConfirmModal == true){
             this.buttomDisabled = true;
             this.loading = true;
             this.conversaCompleta = [];
@@ -299,7 +363,7 @@ export default class ChatIA extends NavigationMixin(LightningElement) {
                     variant: 'error'
                 }));
             }
-            if(closeConfirmModal != true){
+            if(showToast != true){
                 try{
                     const response = await deleteChatById({backupChat: this.backupChat});
                     this.dispatchEvent(new ShowToastEvent({
@@ -316,11 +380,35 @@ export default class ChatIA extends NavigationMixin(LightningElement) {
                 }
             }
         }
-
         this.buttomDisabled = false;
         this.loading = false;
         const input = this.template.querySelector('lightning-input[data-id="campoMensagem"]');
         setTimeout(() => input.focus());
     }
-   
+
+    likeButton(event){
+        this.conversaCompleta.forEach(mensagem =>{
+            if(mensagem.correlator === event.target.value && mensagem.css == ''){
+                mensagem.css = '--slds-c-icon-color-foreground: #660099;margin-left: -20px;';
+                this.correlator = event.target.value;
+                mensagem.disableDislike = true;
+                this.feedbackIsOpen = true;
+                this.dislikeDisabled = true;
+                this.feedbackValue = 'positive';
+            }
+        })
+    }
+    
+    dislikeButton(event){
+        this.conversaCompleta.forEach(mensagem =>{
+            if(mensagem.correlator === event.target.value && mensagem.css == ''){
+                mensagem.css = '--slds-c-icon-color-foreground: #660099;margin-left: -20px;';
+                this.correlator = event.target.value;
+                mensagem.disableLike = true;
+                this.feedbackIsOpen = true;
+                this.likeDisabled = true;
+                this.feedbackValue = 'negative';
+            }
+        })
+    }
 }
