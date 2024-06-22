dee9a4c1105b62f9a447760bcddbf389017f483d
80841110@telefonicati.onmicrosoft.com <80841110@telefonicati.onmicrosoft.com>
2024-06-22 17:29:18 -0300

    fix: LWC de botão de antecipação

--- force-app/main/default/lwc/cfValPrePaidAssetDetailsButtons_AnteciparButton/styleDefinition.js
+++
@@ -1,3 +0,0 @@
-let styleDefinition =
-      {"state0element0":[{"conditions":"default","styleObject":{"class":"slds-col  condition-element slds-text-align_center slds-border_top slds-border_right slds-border_bottom slds-border_left slds-p-top_xx-small slds-m-right_x-small editButton slds-size_12-of-12  ","style":"     border-top: #cccccc 1px solid;border-right: #cccccc 1px solid;border-bottom: #cccccc 1px solid;border-left: #cccccc 1px solid; \n    border-radius:5px;     width: 35px;","styleProperties":{"iconSize":"small","iconColor":"#dddbda"}}}]};
-  export default styleDefinition
