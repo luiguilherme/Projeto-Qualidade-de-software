b6901710bcc573706fa87855a4ead16ab1ceffb9
80841110@telefonicati.onmicrosoft.com <80841110@telefonicati.onmicrosoft.com>
2024-06-23 01:14:08 -0300

    fix: LWC PortabilityActionsPage

---
+++ force-app/main/default/lwc/cfValPortabilityActionsPage/cfValPortabilityActionsPage.js
@@ -0,0 +1,96 @@
+import { FlexCardMixin } from "vlocity_cmt/flexCardMixin";
+    import { CurrentPageReference } from 'lightning/navigation';
+    import {interpolateWithRegex, interpolateKeyValue, loadCssFromStaticResource } from "vlocity_cmt/flexCardUtility";
+    
+          import { LightningElement, api, track, wire } from "lwc";
+          import pubsub from "vlocity_cmt/pubsub";
+          import { getRecord } from "lightning/uiRecordApi";
+          import { OmniscriptBaseMixin } from "vlocity_cmt/omniscriptBaseMixin";
+          import data from "./definition";
+          
+          import styleDef from "./styleDefinition";
+              
+          export default class cfValPortabilityActionsPage extends FlexCardMixin(OmniscriptBaseMixin(LightningElement)){
+              currentPageReference;        
+              @wire(CurrentPageReference)
+              setCurrentPageReference(currentPageReference) {
+                this.currentPageReference = currentPageReference;
+              }
+              @api debug;
+              @api recordId;
+              @api objectApiName;
+              @track _omniSupportKey = 'cfValPortabilityActionsPage';
+                  @api get omniSupportKey() {
+                    return this._omniSupportKey;
+                  }
+                  set omniSupportKey(parentRecordKey) {
+                    this._omniSupportKey = this._omniSupportKey  + '_' + parentRecordKey;
+                  }
+              @track record;
+              @track _sessionApiVars = {};
+        @api set cfCustomerInteraction(val) {
+          if(typeof val !== "undefined") {
+            this._sessionApiVars["CustomerInteraction"] = val;
+          }
+        } get cfCustomerInteraction() {
+          return this._sessionApiVars["CustomerInteraction"] || "{vlocity_cmt__CustomerInteraction__c}";
+        }
+      
+              
+              pubsubEvent = [];
+              customEvent = [];
+              
+              connectedCallback() {
+                super.connectedCallback();
+                this.setThemeClass(data);
+                this.setStyleDefinition(styleDef);
+                data.Session = {} //reinitialize on reload
+                
+                this.flexiPageWidthAwareCB = this.flexiPageWidthAware.bind(this);
+                  window.addEventListener('resize', this.flexiPageWidthAwareCB);
+                
+                this.setDefinition(data);
+ this.registerEvents();
+                
+                
+              }
+              
+              disconnectedCallback(){
+                super.disconnectedCallback();
+                    this.omniSaveState(this.records,this.omniSupportKey,true);
+                    window.removeEventListener('resize', this.flexiPageWidthAwareCB);
+
+                  this.unregisterEvents();
+              }
+
+              registerEvents() {
+                
+        this.pubsubEvent[0] = {
+          [interpolateWithRegex(`data`,this._allMergeFields,this._regexPattern,"noparse")]: this.handleEventAction.bind(this, data.events[0],0)
+        };
+        this.pubsubChannel0 = interpolateWithRegex(`omniscript_action`,this._allMergeFields,this._regexPattern,"noparse");
+        pubsub.register(this.pubsubChannel0,this.pubsubEvent[0]);
+
+        this.pubsubEvent[1] = {
+          [interpolateWithRegex(`RefreshPage`,this._allMergeFields,this._regexPattern,"noparse")]: this.handleEventAction.bind(this, data.events[1],1)
+        };
+        this.pubsubChannel1 = interpolateWithRegex(`valPortabilityActionsPage`,this._allMergeFields,this._regexPattern,"noparse");
+        pubsub.register(this.pubsubChannel1,this.pubsubEvent[1]);
+
+              }
+
+              unregisterEvents(){
+                pubsub.unregister(this.pubsubChannel0,this.pubsubEvent[0]);
+pubsub.unregister(this.pubsubChannel1,this.pubsubEvent[1]);
+
+              }
+            
+              renderedCallback() {
+                super.renderedCallback();
+                
+                if(!this.containerWidthInitialised) {
+                  this.containerWidthInitialised = true;
+                  this.flexiPageWidthAware();
+                }
+              }
+          }
