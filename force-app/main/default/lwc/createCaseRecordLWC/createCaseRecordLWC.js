fae6ba5bb66cfcd4b9db64ed3b9f9e08a8655f30
Cecilia Bastos de Souza <80944489@telefonicati.onmicrosoft.com>
2024-10-15 14:44:19 -0300

    PTI4349-126 Resolves Realocação do Botão "Criar" para Abertura Manual de Casos (OmniStudio)

--- force-app/main/default/lwc/createCaseRecordLWC/createCaseRecordLWC.js
+++ force-app/main/default/lwc/createCaseRecordLWC/createCaseRecordLWC.js
@@ -1,25 +1,31 @@
 import { LightningElement, api } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
-import {encodeDefaultFieldValues} from 'lightning/pageReferenceUtils';
+import { encodeDefaultFieldValues } from 'lightning/pageReferenceUtils';
 
 export default class CreateCaseRecordLWC extends NavigationMixin(LightningElement) {
     @api recordId;
     @api param;
     AccountId;
     ContactId;
+    CustomerInteractionId;
+    CustomerInteractionTopicId;
     
     connectedCallback(){
         let forceParamObject = JSON.parse(JSON.stringify(this.param))[0];
-        const { AccountId, vlocity_cmt__PrimaryContactId__c: ContactId} = forceParamObject;
+        const { AccountId, vlocity_cmt__PrimaryContactId__c: ContactId, VarContextId:CustomerInteractionId, TopicId:CustomerInteractionTopicId} = forceParamObject;
 
         this.AccountId = (AccountId !== undefined && AccountId !== "") ? AccountId : null;
         this.ContactId = (ContactId !== undefined && ContactId !== "") ? ContactId : null;
+        this.CustomerInteractionId = (CustomerInteractionId !== undefined && CustomerInteractionId !== "") ? CustomerInteractionId : null;
+        this.CustomerInteractionTopicId = (CustomerInteractionTopicId !== undefined && CustomerInteractionTopicId !== "") ? CustomerInteractionTopicId : null;
     }
 
     createNewCase() {
         const defaultValueCase = encodeDefaultFieldValues({
             AccountId: this.AccountId,
-            ContactId: this.ContactId
+            ContactId: this.ContactId,
+            CustomerInteractionId__c: this.CustomerInteractionId,
+            CustomerInteractionTopic__c: this.CustomerInteractionTopicId
         })
         
         this[NavigationMixin.Navigate]({
