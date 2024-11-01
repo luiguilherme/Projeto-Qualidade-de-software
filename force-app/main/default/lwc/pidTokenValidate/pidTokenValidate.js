cf6a1d3a6dea022dc6bb88133212d67bc7f1d7aa
Marsal Vaz <marsal.vaz@telefonica.com>
2024-11-01 15:53:10 -0300

    PRB0057392 - Validação de token sendo executada uma vez por segundo

--- force-app/main/default/lwc/pidTokenValidate/pidTokenValidate.js
+++ force-app/main/default/lwc/pidTokenValidate/pidTokenValidate.js
@@ -222,7 +222,7 @@ export default class PidTokenValidate extends LightningElement {
 
             parentThis.refreshData();
             parentThis.timeMiliseconds -= 1000;
-        }, 1000);
+        }, 5000);
         
         
     }
